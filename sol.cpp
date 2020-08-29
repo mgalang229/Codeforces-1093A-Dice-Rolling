94826c4b9ba4f99a5dce54ba85e162bcabc1cc78
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-08-29 13:25:58 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--) {
+		int x;
+		cin >> x;
+		cout << x/2 << "\n";
+	}
+}
